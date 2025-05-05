448bed8adbc29e6bde6dcd1f09e391f9c8a20e7f
Sten-Erik <the.sten@windowslive.com>
2025-05-05 20:19:07 +0300

    Added ItemInformation and RecipeInformation classes.

---
+++ BackEnd/AACBackEnd/Models/ItemInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace AACBackEnd.Models
+{
+    public class ItemInformation
+    {
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public double ItemCost { get; set; }
+    }
+}
